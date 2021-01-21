dcccb3a00de5092105a727e865bc1c10f0b94d49
emrahkk <emrahkk@gmail.com>
2021-01-21 17:12:21 +0300

    add kii, cpuhog, memhog endpoints

--- server.js
+++ server.js
@@ -17,16 +17,16 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/osinfo',(req,res)=>{
+app.get('/osinfo', (req, res) => {
   res.json(getOsInfo());
 })
 
-app.get('/env',(req,res)=>{
+app.get('/env', (req, res) => {
   res.json(process.env);
 })
 
 // Ready 
-app.get('/ready',(req,res)=>{
+app.get('/ready', (req, res) => {
   let uptime = process.uptime()
   if (uptime < 30) {
     res.status(404).json({
@@ -41,12 +41,46 @@ app.get('/ready',(req,res)=>{
   }
 })
 
-// Caos
-app.get('/caos',(req,res)=>{
+// 
+app.get('/kill', (req, res) => {
+  console.log("Killing server");
+  process.exit(1);
+})
+
+// memory hog
+app.get('/cpuhog', (req, res) => {
+  while (true) {}
+})
+
+// memory hog
+const requests = new Map();
+app.get('/memhog', (req, res) => {  
+  requests.set(req.id, req);
+  memusage = process.memoryUsage();
   res.json({
-    message: "Ther will be caos!!!"
+    rss: memusage.rss,
+    heapTotal: memusage.heapTotal,
+    heapUsed: memusage.heapUsed,
+    external: memusage.external,
+    arrayBuffers: memusage.arrayBuffers
   })
 })
+
+app.get('/isprime', (req, res) => {
+  let number = req.query.number;
+  if (checkPrime(number)) {
+    res.json({
+      message: number+" is a prime number"
+    })
+  } else {
+    res.json({
+      message: number+" is NOT a prime number"
+    })
+  }
+})
+
+
+
 
 // Utility functions
 const getOsInfo = () => {
@@ -58,6 +92,22 @@ const getOsInfo = () => {
     arch: os.arch(),
     platform: os.platform()
   };
+}
+
+const checkPrime = (n) => {
+  if (n === 1) {
+    return false;
+  }
+  else if (n === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 //
